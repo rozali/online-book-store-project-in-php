e1ff67431730fcbf23c173513446fb48a6b3a7d7
Rozali Syahputra <rozali.putra@gmail.com>
2018-05-14 22:26:45 +0700

    moving IDR function

--- functions/cart_functions.php
+++ functions/cart_functions.php
@@ -32,9 +32,4 @@
 		}
 		return $items;
 	}
-
-	function rupiah($angka){
-		$hasil_rupiah = "Rp " . number_format($angka,2,',','.');
-		return $hasil_rupiah;
-	}
 ?>
--- functions/database_functions.php
+++ functions/database_functions.php
@@ -122,4 +122,9 @@
 		}
 		return $result;
 	}
+
+	function rupiah($angka){
+		$hasil_rupiah = "Rp " . number_format($angka,2,',','.');
+		return $hasil_rupiah;
+	}
 ?>
