d6714775e85300bd0328082841615730ee443e06
rozali <rozali.putra@gmail.com>
2018-05-16 13:55:07 +0700

    adding FA, editing a few element and text

--- template/header.php
+++ template/header.php
@@ -10,6 +10,7 @@
     <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="./bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
     <link href="./bootstrap/css/jumbotron.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
 
   <body>
@@ -30,13 +31,13 @@
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
               <!-- link to publiser_list.php -->
-              <li><a href="publisher_list.php"><span class="glyphicon glyphicon-paperclip"></span>&nbsp; Publisher</a></li>
+              <li><a href="publisher_list.php"><span class="glyphicon glyphicon-paperclip"></span>&nbsp; Penerbit</a></li>
               <!-- link to books.php -->
-              <li><a href="books.php"><span class="glyphicon glyphicon-book"></span>&nbsp; Books</a></li>
+              <li><a href="books.php"><span class="glyphicon glyphicon-book"></span>&nbsp; Buku</a></li>
               <!-- link to contacts.php -->
-              <li><a href="contact.php"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp; Contact</a></li>
+              <!-- <li><a href="contact.php"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp; Contact</a></li> -->
               <!-- link to shopping cart -->
-              <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; My Cart</a></li>
+              <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; Keranjang</a></li>
             </ul>
         </div>
       </div>
@@ -47,7 +48,7 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1>Welcome to online Global Science bookstore</h1>
+        <h1>Global Science</h1>
         <!-- <p class="lead">This site has been made using PHP with MYSQL (procedure functions)!</p>
         <p>The layout use Bootstrap to make it more responsive. It's just a simple web!</p> -->
       </div>
