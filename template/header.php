e09f8935b06243984480669515a530ecdf869313
rozali <rozali.putra@gmail.com>
2018-05-24 15:52:06 +0700

    deleting a few css cdn

--- template/header.php
+++ template/header.php
@@ -12,9 +12,6 @@
     <link href="./bootstrap/css/jumbotron.css" rel="stylesheet">
     <link href="./bootstrap/css/cust-element.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
   </head>
 
   <body>
