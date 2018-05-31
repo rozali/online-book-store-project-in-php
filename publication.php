66ad341714567567d3dbfdbed3259cc7c029ad88
rozali <rozali.putra@gmail.com>
2018-05-31 14:27:14 +0700

    new publication page!

---
+++ publication.php
@@ -0,0 +1,35 @@
+<?php
+  $title = "Publikasi Buku Ajar";
+  require_once "./template/header.php";
+?>
+<div class="row">
+  <div class="col-lg-12">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        Publikasi Buku Ajar
+      </div>
+      <div class="panel-body">
+        <p class="lead"><strong>Penerbit Global Science</strong> saat ini menyediakan jasa penerbitan buku ajar yang terpercaya. Penerbitan buku bertujuan untuk membantu menyebar luaskan karya-karya dosen di Indonesia dan dapat digunakan untuk syarat administratif dosen, mendapatkan angka kredit kumulatif (KUM) dll. Selain itu, Penerbitan buku di Global Science memiliki keunggulan karena proses cepat dan terjangkau. Penerbitan buku yang baik adalah dengan melakukan proses telaah terlebih dulu sehingga ketika dicetak tidak ada kesalahan penulisan dalam buku. Kami sangat menjaga kualitas dan profesionalisme dalam proses publikasi buku ajar nasional. </p>
+        <div class="well">
+          <p>Penerbit Global Science bertanggung jawab mempersiapkan:</p>
+          <ol>
+            <li>Pembuatan Cover Buku</li>
+            <li>Proses Proofreading dan Editing Profesional</li>
+            <li>Penataan layout buku agar lebih menarik</li>
+            <li>Pengurusan ISBN</li>
+            <li>Pencetakan Buku ber-ISBN (2 Eksemplar)</li>
+          </ol>
+          <p class="text-primary">Total Biaya: <abbr title="Sudah termasuk semua layanan">Rp.1000.000,-</abbr></p>
+          <p class="text-primary">Perkiraan Waktu selesai: <abbr title="Sudah termasuk semua layanan">1 Bulan (Maksimal)</abbr></p>
+        </div>
+        <blockquote>
+          <p>Kami siap bekerja sama dengan semua dosen di seluruh Indonesia.</p>
+          <p>Semoga bisa terus berkarya demi kemajuan bangsa Indonesia.</p>
+        </blockquote>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+  require_once "./template/footer.php";
+?>
